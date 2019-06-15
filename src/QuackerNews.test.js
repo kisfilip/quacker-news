b6bcf3b44e2f40e73d2f5daff55e791e9fb88c24
Filip Kis <filip.kis.dev@gmail.com>
2019-06-15 13:49:05 +0200

    Format basic data structure and rename App to QuackerNews

--- src/QuackerNews.test.js
+++ src/QuackerNews.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import QuackerNews from './QuackerNews';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<QuackerNews />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
