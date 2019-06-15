d2f702a348eb64a4167916e8f84461fe3a1ed12f
Filip Kis <filip.kis.dev@gmail.com>
2019-06-15 20:48:45 +0200

    Implement 'show next 30 posts' button using the react-router-dom Link element

--- src/components/TopStoriesContainer.js
+++ src/components/TopStoriesContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import {withRouter} from 'react-router';
+import {Link} from 'react-router-dom';
 import fetchTopStoriesObjs from './api/fetchTopStoriesObjs.js';
 
 import StoryListItem from './StoryListItem';
+
 class TopStoriesContainer extends React.Component {
   state = {
     topStoriesObjs: null
@@ -17,6 +19,7 @@ class TopStoriesContainer extends React.Component {
 
   render() {
     console.log(this.state)
+    console.log(this.props)
     let storiesList
     if (this.state.topStoriesObjs) {
       storiesList = this.state.topStoriesObjs.map(storyObj => {
@@ -28,14 +31,18 @@ class TopStoriesContainer extends React.Component {
         );
       });
     };
+    let urlMatch = "page2";
+    if (this.props.match.params.id) {
+      urlMatch = "page" + (Number(this.props.match.params.id) + 1);
+    }
     return (
       <div>
         TopStoriesContainer
         { storiesList }
-        <button> Next 30 items </button>
+        <Link to={urlMatch}> Next 30 items </Link>
       </div>
     );
   }
 }
 
-export default TopStoriesContainer;
+export default withRouter(TopStoriesContainer);
