266e277e57566bd81e936509368771caf8e89669
Filip Kis <filip.kis.dev@gmail.com>
2019-06-16 16:36:55 +0200

    Add jsx elements for StoryListItem

--- src/components/StoryListItem.js
+++ src/components/StoryListItem.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 class StoryListItem extends React.Component {
   render() {
-    const {title, by, kids, score} = this.props.storyObj;
+    const {id, title, by, descendants, score, url} = this.props.storyObj;
     const orderNum = this.props.orderNum;
     return (
       <div className="Story-list-item">
-        <div>
+        <aside>
           {orderNum}
-        </div>
-        <div>
-        {title}{by}{kids}{score}
-        </div>
+        </aside>
+        <article>
+          <div>
+            <h1>
+              {title}
+            </h1>
+            <a href={url} target="_blank">
+              ({url})
+            </a>
+          </div>
+          <div>
+            <span>
+              {score} points by {by}
+            </span>
+            <span>
+              |
+            </span>
+            <Link to={`/story/${id}`}>
+              {descendants} comments
+            </Link>
+          </div>
+        </article>
       </div>
     );
   }
