5b43c9a7551b9d6771b9473428855172f7e48d2d
Filip Kis <filip.kis.dev@gmail.com>
2019-06-16 12:14:30 +0200

    Add order number for StoryListItem; Add and format comments to better describe TopStoriesContainer functionality

--- src/components/TopStoriesContainer.js
+++ src/components/TopStoriesContainer.js
@@ -11,28 +11,41 @@ class TopStoriesContainer extends React.Component {
     pageId: null
   }
 
+  // Initial fire when topStoriesIds are recieved.
+  // Successive fire when url/path changes.
   componentDidUpdate() {
-    let pageNum = 1;
+    let pageNum = 1; // Initial display page.
+
     if (this.props.match.params.id) {
-      pageNum = Number(this.props.match.params.id);
+      pageNum = Number(this.props.match.params.id); // Set display page via url/path.
     }
-    if (this.state.pageId === null || this.state.pageId !== this.props.match.params.id) {
+
+    if (this.state.pageId !== this.props.match.params.id) {
       fetchTopStoriesObjs(this.props.topStoriesIds, pageNum)
         .then(resolved => this.setState({
           topStoriesObjs: resolved,
+
+          // Prevents componentDidUpdate infinite loop.
+          // Initial opening of home page sets pageId to undefined.
           pageId: this.props.match.params.id
         }));
     }
   }
 
   render() {
-    let storiesList
+    let storiesList;
+
     if (this.state.topStoriesObjs) {
+      let orderNum = (this.state.pageId - 1) * 30; // Used for ordering StoryListItem-s.
+      if (!orderNum) {orderNum = 0}; // Prevents undefined value on initial app load.
+
       storiesList = this.state.topStoriesObjs.map(storyObj => {
+        orderNum++;
         return (
           <StoryListItem
             storyObj={storyObj}
             key={storyObj.id}
+            orderNum={orderNum}
           />
         );
       });
@@ -41,15 +54,15 @@ class TopStoriesContainer extends React.Component {
     if (this.props.match.params.id) {
       urlMatch = "page" + (Number(this.props.match.params.id) + 1);
     }
-    // TODO: disable link button via css pointer-events
+    // TODO: Disable link button via css pointer-events
     //       until page loads to prevent stacking api calls
     //       (the current solution works but for example -
     //       if a user clicks the 'next 30 items' button
     //       multiple times in a row (until the page has finished loading),
-    //       the consequent clicks will stack api calls on top of each other)
-    // TODO: navigating back should load previously fetched objects instead
+    //       the consequent clicks will stack api calls on top of each other).
+    // TODO: Navigating back should load previously fetched objects instead
     //       of fetching them again - this can be solved by pushing new objects
-    //       to state, and getting them from state when navigating back
+    //       to state, and getting them from state when navigating back.
     return (
       <div>
         TopStoriesContainer
